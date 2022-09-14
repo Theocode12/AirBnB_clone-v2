01c46d516f9c64056ccef0608165166c91ad2abb
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-14 02:10:17 +0100

    add places

--- models/user.py
+++ models/user.py
@@ -16,6 +16,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = Relationship(
+            "Place", cascade="all, delete", backref="user")
         reviews = Relationship(
             "Review", cascade="all, delete", backref="user")
     else:
