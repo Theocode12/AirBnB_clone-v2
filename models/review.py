dc75a56fe44748ab4c291a70542ab06f06a11389
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-15 05:57:26 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -24,3 +24,8 @@ class Review(BaseModel, Base):
         place_id = ""
         user_id = ""
         text = ""
+        
+        
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
