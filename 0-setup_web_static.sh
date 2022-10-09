b1fffb85ea07b193c739032d464720ae09a23f6a
Theocode12 <vchidubem9@gmail.com>
2022-10-09 19:02:07 +0100

    web-static setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-# A Bash script that sets up your web servers for the deployment of web_static
+# configires Nginx server with deploymeny folders
 apt-get update -y
-apt-get install nginx -y
-#mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+apt-get install -y nginx
 mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo "Holberton School" | /data/web_static/releases/test/index.html
+echo "<marquee> Let's get it </marquee>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
+# Write after the error declaration
 sed -i "/error_page 404 \/404.html;/a \\\n\tlocation /hbnb_static { \
-	\n\t\talias /data/web_static/current/; \
-	\n\t\tautoindex off; \
-	\n\t}" /etc/nginx/sites-available/default
-#sed -i '/displaying a 404./ a\\t\talias /data/web_static/current/;' /etc/nginx/sites-enabled/default
-
+	        \n\t\talias /data/web_static/current/; \
+		        \n\t\tautoindex off; \
+			        \n\t}" /etc/nginx/sites-available/default
+# restart nginx
 service nginx restart
