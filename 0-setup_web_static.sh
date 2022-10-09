65ca03fb0e50bf6e87e7fa9abdd49ae4fbd087d3
Theocode12 <vchidubem9@gmail.com>
2022-10-09 16:59:49 +0100

    web-static setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 # A Bash script that sets up your web servers for the deployment of web_static
-
-apt install nginx -y
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-#echo 'Holberton School' | sudo tee -a /data/web_static/releases/test/index.html
-
-if [ -L /data/web_static/current ]
-then
-	rm /data/web_static/current
-fi
-
-ln -s /data/web_static/releases/test/ /data/web_static/current
+apt-get update -y
+apt-get install nginx -y
+mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo 'Holberton School' | /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
 #sed -i '/displaying a 404./ a\\t\talias /data/web_static/current/;' /etc/nginx/sites-enabled/default
