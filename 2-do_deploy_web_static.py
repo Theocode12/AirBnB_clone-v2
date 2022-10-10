a67b5d988bbf9f509c90dc305a1c59b5fcf2aae0
Theocode12 <vchidubem9@gmail.com>
2022-10-10 16:23:23 +0100

    deploy webstatic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,11 +26,11 @@ def do_deploy(archive_path):
 
         put(remote_path=tmp_path, local_path=archive_path)
         run("mkdir -p {}".format(release_path))
-        run("tar -xvf {} -C {}".format(tmp_path, release_path))
+        run("tar -xvzf {} -C {}".format(tmp_path, release_path))
         run("mv {}/web_static/* {}".format(release_path, release_path))
         run("rm -r {}/web_static".format(release_path))
         run("rm -r {}".format(tmp_path))
-        run("rm -r /data/web_static/current")
+        run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(release_path))
         return True
     except Exception:
