dc12c07489d4bb8c5742bd448941a8317ff4ece2
Theocode12 <vchidubem9@gmail.com>
2022-10-17 07:13:12 +0100

    cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+States rendering
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """
+    tears-down session after each request
+    """
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def state_cites_page():
+    """
+    render list of states
+    """
+    states = storage.all(State).values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
